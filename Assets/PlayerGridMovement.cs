e1f829e44f4665c894a58cdf3720bee8d03c0416
shshwdr <cs.vivian04@gmail.com>
2022-01-31 00:08:18 -0600

    movement on grid

--- Assets/PlayerGridMovement.cs
+++ Assets/PlayerGridMovement.cs
@@ -9,10 +9,11 @@ public class PlayerGridMovement : MonoBehaviour
     public Transform movePoint;
     public LayerMask colliderMask;
     public float moveAllowDistance = 0.05f;
+    public float moveStepDistance = 0.16f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
@@ -21,7 +22,23 @@ public class PlayerGridMovement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
         if (Vector3.Distance(transform.position, movePoint.position) < moveAllowDistance)
         {
-
+            var horizonMovement = Input.GetAxisRaw("Horizontal");
+            var verticalMovement = Input.GetAxisRaw("Vertical");
+            if (Mathf.Abs(horizonMovement) == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizonMovement* moveStepDistance, 0, 0), .1f, colliderMask))
+                {
+                    movePoint.position += new Vector3(horizonMovement* moveStepDistance, 0, 0);
+                }
+            }
+            if (Mathf.Abs(verticalMovement) == 1)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0, verticalMovement* moveStepDistance,  0), .1f, colliderMask))
+                {
+                    movePoint.position += new Vector3(0, verticalMovement* moveStepDistance, 0);
+                }
+            }
         }
+        Debug.Log("movement position " + movePoint.position);
     }
 }
